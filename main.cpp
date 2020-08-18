d575210270ffef7d399a47bbaed54df65c3a0c18
Ariadna_Ch <ariadna.c2405@gmail.com>
2020-08-18 09:22:54 -0600

    Cambios en el main, agreguegué plan de alimentos a un paciente

--- main.cpp
+++ main.cpp
@@ -8,21 +8,27 @@
 
 int main() {
 
+
+    cout<<"Informacion de los pacientes"<<endl;
+    cout<<"--------------------------------------------"<<endl;
     Paciente* pacienteSano = new PacienteSano("Tiago","Masculino","Mejorar niveles de azucar", new PlanAlimentos("06/11/2019"));
     Paciente* pacienteEnfermo = new PacienteDiabetico(2, 2, 4, "Angelo", "Masculino", new PlanAlimentos("14/8/18"));
     Paciente* pacienteEnfermo2 = new PacienteDiabetico(1,1,3,"Ariadna", "Femenino", new PlanAlimentos("17/6/20"));
 
    pacienteSano->agregaAlimento(new Alimento("Sandia", 5, 3));
-   pacienteSano->agregaAlimento(new Alimento("Manzana", 2, 1));
    pacienteEnfermo->agregaAlimento(new Alimento("Ensalada", 3, 0));
-
+    pacienteEnfermo2->agregaAlimento(new Alimento("Manzana", 2, 1));
+    cout<<"Paciente #1"<<endl;
     cout << "Paciente sano:\n" << pacienteSano->toString()<<endl;
     cout << pacienteSano->planAlimentos()<<endl;
-
+    cout<<"Paciente #2"<<endl;
     cout << "Paciente diabetico:\n" << pacienteEnfermo->toString()<<endl;
     cout << pacienteEnfermo->planAlimentos()<<endl;
-
+    cout<<"Paciente #3"<<endl;
     cout << "\nPaciente diabetico 2:\n" << pacienteEnfermo2->toString();
+    cout << pacienteEnfermo2->planAlimentos()<<endl;
+    cout << pacienteEnfermo2->planAlimentos()<<endl;
+
 
 
     delete pacienteSano;
